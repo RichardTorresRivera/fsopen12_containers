7d358797fcebe206d3d8f947496321fe75824ea7
RichardTorresRivera <rmtr2711@gmail.com>
2026-03-11 10:38:11 -0500

    ex. 17 - nginx reverse proxy server in todo-frontend

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['localhost', 'app']
+  },
   test: {
     globals: true,
     environment: 'jsdom',
